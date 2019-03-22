0b56b814cdbbb08878534cdb5bddc7718f3f3133
Alastair <alastair.slater@mclaren.com>
2019-03-22 10:52:53 +0000

    Added date padding CSS

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -21,7 +21,7 @@ class BlogRoll extends React.Component {
                     {post.frontmatter.title}
                   </Link>
                   <span> &bull; </span>
-                  <span className="subtitle is-size-5 is-block">
+                  <span className="subtitle is-size-5 is-block date-display">
                     {post.frontmatter.date}
                   </span>
                 </p>
